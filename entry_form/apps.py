3e03d4e134a553ba8fd3a8ce980dc5966bedc61e
Sina Arasteh <sinna.arasteh@gmail.com>
2024-10-24 16:30:22 +0330

    Create the 'forms.py' file and transfer all the codes from the 'models.py' to the 'forms.py'
    I decided to transfer all the codes as I changed my mind to use 'forms.Form' instead of 'forms.ModelForm'.

--- entry_form/apps.py
+++ entry_form/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class EntryFormConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.forms.BigAutoField'
     name = 'entry_form'
