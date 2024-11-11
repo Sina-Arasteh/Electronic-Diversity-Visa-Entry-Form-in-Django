0d8db0b88af2bff9a28c9c5e10ff94e16a487b71
Sina Arasteh <sinna.arasteh@gmail.com>
2024-11-11 10:57:24 +0330

    Create the 'begin_entry' page and its related templates

--- entry_form/tests.py
+++ entry_form/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+class BeginEntryViewTests(TestCase):
+
+    def test_template(self):
+        client = Client()
+        response = client.get('/')
+        self.assertTemplateUsed(response, "entry_form/begin_entry.html")
