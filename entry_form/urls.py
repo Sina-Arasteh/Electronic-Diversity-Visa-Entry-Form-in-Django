01d314d0a60e4ed2453eb16fa3c7b8f872dbeff2
Sina Arasteh <sinna.arasteh@gmail.com>
2024-11-19 17:36:59 +0330

    Initial preparation of 'view.py' file

--- entry_form/urls.py
+++ entry_form/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.begin_entry),
+    path('entry-form', views.entry_form , name="entry_form")
 ]
--- entry_form/views.py
+++ entry_form/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
 
 def begin_entry(request):
     return render(request, 'entry_form/begin_entry.html')
+
+def entry_form(request):
+    if request.method == "POST":
+        pass
+
+    form = forms.EntrantForm()
+    return render(request, "entry_form/entry_form.html", {
+        "form": form
+    })    
+
